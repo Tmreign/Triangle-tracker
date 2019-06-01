435f36fc17454c7aa7078978675b95ff58a79a5d
tmreign <tomwanyoike9@gmail.com>
2019-06-01 10:05:37 +0300

    initialize

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var number1 =parseInt(prompt("Enter number"));
+var number2 =parseInt(prompt("Enter number"));
+var number3 =parseInt(prompt("Enter number"));
+
+ if(number1==number2 && number2==number3 && number3==number1){
+     alert("equilateral");
+ }
+
+ else if(number1== number2 ||number2==3||number3==1){
+ alert("issocles");
+ }
+ 
+ else { alert("scalene")}
